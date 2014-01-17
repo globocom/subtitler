d847640afe43797d07afdfc7643396a1df481568
Mateus Del Bianco <mateus@delbianco.net.br>
2014-01-17 17:18:36 -0200

    makes parser identify blocks in .sub files

--- lib/subtitler.rb
+++ lib/subtitler.rb
@@ -1,5 +1,30 @@
 require "subtitler/version"
 
 module Subtitler
-  # Your code goes here...
+  module Parser
+    def parse input
+      # 10:01:28.15\10:01:31.11
+      # hh:mm:ss.ff\hh:mm:ss.ff
+      blocks = []
+
+      input.each_line do |line|
+        if line.match(/(\d{2}:\d{2}:\d{2}[\.\:]\d{2})\\(\d{2}:\d{2}:\d{2}[\.:]\d{2})/)
+          blocks << line
+        end
+      end
+      Subtitle.new(blocks: blocks)
+    end
+  end
+  extend Parser
+
+  class Subtitle
+    attr_accessor :blocks
+
+    def initialize(blocks: [])
+      @blocks = blocks
+    end
+  end
+
+  class SubtitleBlock
+  end
 end
--- spec/subtitler_spec.rb
+++ spec/subtitler_spec.rb
@@ -1,6 +1,24 @@
 require "spec_helper"
 
 describe Subtitler do
+  describe "parsing input" do
+    context "Sofni (.sub) files" do
+      subject do
+        Subtitler.parse(
+          <<-EOF.strip_heredoc
+            -Qual é, meu irmão?
+            -Minha bicicleta!
+            10:01:28.15\\10:01:31.11
+            Nem sonhando, primo!
+            10:03:10.26\\10:03:12.24
+          EOF
+        )
+      end
+
+      it { expect(subject.blocks).to have(2).items }
+    end
+  end
+
   describe "converting input formats to SubRip (.srt)" do
     it "works"
   end
