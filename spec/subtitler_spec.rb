9720184b09afd0416e1210c17145c92d6354120a
Mateus Del Bianco <mateus@delbianco.net.br>
2014-01-17 16:04:57 -0200

    some pending tests

---
+++ spec/subtitler_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe Subtitler do
+  describe "converting input formats to SubRip (.srt)" do
+    it "works"
+  end
+
+  describe "time-shifting subtitles" do
+    it "works"
+  end
+
+  describe "validating input files" do
+    it "works"
+  end
+end
